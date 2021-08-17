a6bc59d830c323ce10c2f2e0a8157fba39f472d1
Ali Rahhal <arahal81@gmail.com>
2021-08-17 17:44:41 +0300

    lab18

--- tests/test_caesar_cipher.py
+++ tests/test_caesar_cipher.py
@@ -1,5 +1,30 @@
-from caesar_cipher import __version__
+import pytest
+from caesar_cipher.caesar_cipher import crack, decrypt, encrypt
+
+# encrypt a string with a given shift
+# decrypt a previously encrypted string with the same shift
+# encryption should handle upper and lower case letters
+# encryption should allow non-alpha characters but ignore them, including white space
 
 
-def test_version():
-    assert __version__ == '0.1.0'
+@pytest.mark.parametrize(
+    "input,expected_value,key",
+    [
+        ("I love syrup on my pancakes.", "I love syrup on my pancakes.", 3),
+        ("He ran all the way to the county fair.",
+         "He ran all the way to the county fair.", 15),
+        ("He ran all the way to the county fair. Do You?",
+         "He ran all the way to the county fair. Do You?", 6),
+        ("He Ran All The Way To The County Fair.",
+         "He Ran All The Way To The County Fair.", 9),
+    ],
+)
+def test_encrypt_decrypt(input, expected_value, key):
+
+    assert expected_value == decrypt(encrypt(input, key), key)
+
+
+# decrypt encrypted version of It was the best of times, it was the worst of times. WITHOUT knowing the shift used.
+def test_crack():
+    encrypted = encrypt("it was the worst of times", 14)
+    assert crack(encrypted) == "it was the worst of times"
